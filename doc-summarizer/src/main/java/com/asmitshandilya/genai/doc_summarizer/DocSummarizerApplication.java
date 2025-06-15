306c1333824ae241c00e42e14e44042f2e5f94b5
Asmit <asmitbuddy@gmail.com>
2025-06-16 00:59:00 +0530

    Added Application configuration and dependencies

--- doc-summarizer/src/main/java/com/asmitshandilya/genai/doc_summarizer/DocSummarizerApplication.java
+++ doc-summarizer/src/main/java/com/asmitshandilya/genai/doc_summarizer/DocSummarizerApplication.java
@@ -1,13 +1,11 @@
 package com.asmitshandilya.genai.doc_summarizer;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class DocSummarizerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DocSummarizerApplication.class, args);
 	}
-
 }
