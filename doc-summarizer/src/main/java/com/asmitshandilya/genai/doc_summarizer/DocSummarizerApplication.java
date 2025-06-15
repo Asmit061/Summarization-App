8b476bd6a220b2a112fdd6afc5b633554e4b9fd3
Asmit <asmitbuddy@gmail.com>
2025-06-15 21:28:59 +0530

    added the initial files

---
+++ doc-summarizer/src/main/java/com/asmitshandilya/genai/doc_summarizer/DocSummarizerApplication.java
@@ -0,0 +1,13 @@
+package com.asmitshandilya.genai.doc_summarizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocSummarizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocSummarizerApplication.class, args);
+	}
+
+}
